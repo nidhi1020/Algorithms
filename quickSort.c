22ae3d793e85599b9e1efb3d8c2a178ebd306776
nidhi1020 <nidhibiswas111@gmail.com>
2025-09-08 10:17:46 +0600

    quickSort added

---
+++ quickSort.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+// Function to swap two elements
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Partition function
+int partition(int arr[], int low, int high) {
+    int pivot = arr[low];  // taking first element as pivot
+    int i = low + 1;
+    int j = high;
+
+    while (1) {
+        while (i <= high && arr[i] <= pivot) {
+            i++;
+        }
+        while (j >= low && arr[j] > pivot) {
+            j--;
+        }
+        if (i < j) {
+            swap(&arr[i], &arr[j]);
+        } else {
+            break;
+        }
+    }
+    swap(&arr[low], &arr[j]);
+    return j;
+}
+
+// Quick sort function
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int p = partition(arr, low, high);
+        quickSort(arr, low, p - 1);
+        quickSort(arr, p + 1, high);
+    }
+}
+
+// Main function
+int main() {
+    int arr[] = {29, 10, 14, 37, 13, 25, 7, 45, 30, 18};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Before sorting: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    quickSort(arr, 0, n - 1);
+
+    printf("\nAfter sorting: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
